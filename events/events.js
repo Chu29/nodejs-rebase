0bd5b4e2bf8e66d89fd934571062de4763b987ab
Chu A. <chu.amk22@gmail.com>
2026-01-08 10:50:01 +0100

    Emiting and Listening to events

--- events/events.js
+++ events/events.js
@@ -6,16 +6,26 @@ const { EventEmitter } = require("events");
 const myEmitter = new EventEmitter(); // using the new keyword to create an instance of the EventEmitter class
 
 // Using prototypal inheritance to create an EventEmitter
+// Emitting Events
+/**
+ * we call the emit method on the event emitter instance to * emit an event.
+ * Here, "an-event" is the name of the event being emitted, * and some, args are the arguments passed to the event * * * listeners.
+ */
 class MyEmitter extends EventEmitter {
   constructor(opts = {}) {
     super(opts);
     this.name = opts.name;
   }
+  destroy(err) {
+    if (err) this.emit("error", err);
+    this.emit("close");
+  }
 }
 
+// Listening to Events
 
-/**
- * Emitting Events
- */
+const emitter = new MyEmitter({ name: "My Emitter" });
 
-// we call the emit method on the event emitter instance to emit an event
+// register an event listener for the event "close"
+emitter.on("close", () => console.log("close event fired")); // listen for the close event
+emitter.emit("close"); // emit the close event
