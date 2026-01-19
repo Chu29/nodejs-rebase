63992327d8f9ab05dbb8ac8bda34d98718357836
Chu A. <chu.amk22@gmail.com>
2026-01-19 16:09:25 +0100

    working with fs and path modules

---
+++ file-system/fs.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// const { readdirSync, readdir, read } = require("node:fs");
+// const { readdir: readdirProm } = require("node:fs/promises");
+
+// try {
+//   console.log("sync", readdirSync(__dirname));
+// } catch (err) {
+//   console.error(err);
+// }
+
+// readdir(__dirname, (err, files) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log("callback", files);
+// });
+
+// async function run() {
+//   const files = await readdirProm(__dirname);
+//   console.log("promise", files);
+// }
+
+// run().catch((err) => {
+//   console.error(err);
+// });
+
