36d2f26f12d28e38c75dd0769bb4790bac05c003
Chu A. <chu.amk22@gmail.com>
2026-01-21 15:49:23 +0100

    register routes in app.js

--- express-web-server/app.js
+++ express-web-server/app.js
@@ -1,8 +1,13 @@
 " use strict ";
 const express = require("express");
 const createError = require("http-errors");
+const indexRoutes = require("./routes");
+const helloRoutes = require("./routes/hello");
 
 const app = express();
+
+app.use("/", indexRoutes);
+app.use("/hello", helloRoutes);
 
 app.use((req, res, next) => {
   if (req.method !== "GET") {
@@ -12,7 +17,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.send(err.message)
+  res.send(err.message);
 });
 
 module.exports = app;
