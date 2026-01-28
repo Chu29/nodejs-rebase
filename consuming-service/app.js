e87f0e1da68c8554a20426423d5cbe633e5153e8
Chu A. <chu.amk22@gmail.com>
2026-01-28 15:11:52 +0100

    refactor: remove unused path import and update error handling middleware

--- consuming-service/app.js
+++ consuming-service/app.js
@@ -1,24 +1,16 @@
 import express from "express";
 import createError from "http-errors";
-import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
 import indexRouter from "./routes/index.js";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 const app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
@@ -27,15 +19,17 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+// register a error handler middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.status || 500;
+  res.status(statusCode);
+  res.send({
+    error: {
+      status: statusCode,
+      message: err.message,
+      stack: req.app.get("env") === "development" ? err.stack : {},
+    },
+  });
 });
 
 export default app;
